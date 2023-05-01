e7b2afb7ce4bb432ff2106b4592e830a53ef653c
sachacollinson <104627123+sachacollinson@users.noreply.github.com>
2023-05-01 10:44:39 +0100

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 
 
 import msprime
@@ -15,8 +11,6 @@ import math
 from scipy import stats
 
 
-# In[ ]:
-
 
 #General parameters being used 
 mut_rate = 0.000001
@@ -27,9 +21,6 @@ ini_size_ooa = 1000
 ini_size_afr = 10000
 mig_rate = 0
 bneck_time = 1000
-
-
-# In[ ]:
 
 
 #Set up demography model with a population split
@@ -51,16 +42,12 @@ def demography_model(ini_size,growth_rate,sample_size,rec_rate,seq_length, mut_r
     return X_afr, X_ooa, mts
 
 
-# In[ ]:
-
 
 #map corresponding to Linkage Disequilibrium
 def show_corr_mat(X):
     plt.matshow(abs(np.corrcoef(X)), cmap = 'Reds')
     plt.show()
 
-
-# In[ ]:
 
 
 #finding the frequencies of each row of the mutation matrix
@@ -72,11 +59,8 @@ def find_freq(X):
         
     return f
 
-
-# In[ ]:
-
-
-def generate_func_B(freq, func_number): #returns B and list of functional SNPs
+#generate the betas (effect sizes) of the SNPs
+def generate_func_B(freq, func_number): 
     B = []
     func_B = np.sort(np.random.choice([i for i in range(len(freq))],
                               size = func_number, replace = False))
@@ -89,9 +73,6 @@ def generate_func_B(freq, func_number): #returns B and list of functional SNPs
         B.append(np.random.normal(loc = 0.0, scale = sd**2))
     B = np.array(B)
     return B, func_B
-
-
-# In[ ]:
 
 
 #generate the phenotype y=XB+epsilon 
@@ -107,8 +88,6 @@ def y_vector(beta,X,h):
     return y, epsilon, sigmasq
 
 
-# In[ ]:
-
 
 #estimate the heritability in one population from another
 def gaining_h(beta1,X1,h1,X2):
@@ -122,8 +101,6 @@ def gaining_h(beta1,X1,h1,X2):
     return h2
 
 
-# In[ ]:
-
 
 #heritability experiment when fixing the bottleneck population size
 def heritability_n1(N,h1,g_rate,n0_fix):
@@ -135,18 +112,18 @@ def heritability_n1(N,h1,g_rate,n0_fix):
     for i in x:
         h_test=[]
         for j in range(50):
-#generate new population with 0 growth rate
+    #generate new population with 0 growth rate
             if g_rate == 0:
                 X_afr,X_ooa,mts = demography_model(i,0,nsamples,rec_rate,sequence_length,mut_rate,mig_rate)
-#generate new population with growth rate
+    #generate new population with growth rate
             elif g_rate == 1:
                 growth_rate = -math.log(n0_fix/i)/1000
                 X_afr,X_ooa,mts = demography_model(i,growth_rate,nsamples,rec_rate,sequence_length,mut_rate,mig_rate)
-#frequencies, betas and reduced matrix for new population
+    #frequencies, betas and reduced matrix for new population
             freq_ooa = find_freq(X_ooa)
             freq_afr = find_freq(X_afr)
             B_ooa,func_B_ooa = generate_func_B(freq_ooa,int(len(freq_ooa)))
-#betas and reduced matrix for original population
+    #betas and reduced matrix for original population
             B_afr,func_B_afr = generate_func_B(freq_afr,int(len(freq_afr)))
             h_test.append(gaining_h(abs(B_afr),X_afr,h1,X_ooa))
         SE[n] = stats.sem(h_test)
@@ -155,12 +132,10 @@ def heritability_n1(N,h1,g_rate,n0_fix):
     return h,x,SE
 
 
-# In[ ]:
-
 
 #heritability experiment for when fixing the size of the population after t generations
 def heritability_n0(N,h1,g_rate,n1_fix):
-#generate original population with mutation matrix and frequencies
+    #generate original population with mutation matrix and frequencies
     h = []
     x = np.logspace(np.log10(0.1*N), np.log10(10*N), num=30, endpoint=True)
     SE = np.zeros((len(x)))
@@ -168,18 +143,18 @@ def heritability_n0(N,h1,g_rate,n1_fix):
     for i in x:
         h_test=[]
         for j in range(50):
-#generate new population with 0 growth rate
+    #generate new population with 0 growth rate
             if g_rate == 0:
                 X_afr,X_ooa,mts = demography_model(i,0,nsamples,rec_rate,sequence_length,mut_rate,mig_rate)
-#generate new population with growth rate
+    #generate new population with growth rate
             elif g_rate == 1:
                 growth_rate = -math.log(i/n1_fix)/1000
                 X_afr,X_ooa,mts = demography_model(n1_fix,growth_rate,nsamples,rec_rate,sequence_length,mut_rate,mig_rate)
-#frequencies, betas and reduced matrix for new population            
+    #frequencies, betas and reduced matrix for new population            
             freq_ooa = find_freq(X_ooa)
             freq_afr = find_freq(X_afr)
             B_ooa,func_B_ooa = generate_func_B(freq_ooa,int(len(freq_ooa)))
-#betas and reduced matrix for original population
+    #betas and reduced matrix for original population
             B_afr,func_B_afr = generate_func_B(freq_afr,int(len(freq_afr)))
             h_test.append(gaining_h(abs(B_afr),X_afr,h1,X_ooa))
         SE[n] = stats.sem(h_test)
@@ -187,8 +162,6 @@ def heritability_n0(N,h1,g_rate,n1_fix):
         h.append(np.mean(h_test))
     return h,x,SE
 
-
-# In[ ]:
 
 
 #return the effective population size by calculating the harmonic mean
